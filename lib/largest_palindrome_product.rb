00bf748f4dc8e2962a4b78cc83b1fd590ff961ea
github username <github email address>
2017-05-01 16:54:58 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,12 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  max = 0
+  (100..999).each do |first|
+    (100..999).each do |second|
+      product = first * second
+      if product.to_s == product.to_s.reverse && product > max
+        max = product
+      end
+    end
+  end
+  max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,23 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  attr_accessor :range, :answer
+
+  def initialize
+    @range = (100..999).to_a
+    @answer = find_largest_palindrome
+  end
+
+  def find_largest_palindrome
+    max = 0
+    @range.each do |first|
+      @range.each do |second|
+        product = first * second
+        if product.to_s == product.to_s.reverse && product > max
+          max = product
+        end
+      end
+    end
+    max
+  end
+
+
+end
