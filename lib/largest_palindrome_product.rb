f1c4bee70e3a0e36fb1c811ecd01afb13d91a331
dylanito2 <dylanito2@gmail.com>
2017-05-04 13:13:48 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,21 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  range = (100..999)
+  largest = 0
+  range.each do |num|
+    range.each do |num_2|
+      product = num * num_2
+      if product.to_s == product.to_s.reverse
+        largest = product if product > largest
+      end
+    end
+  end
+  largest
+end
+
+
+
+def time
+  start = Time.now
+  yield
+  puts Time.now - start
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,17 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def answer
+
+    range = (100..999)
+    largest = 0
+    range.each do |num|
+      range.each do |num_2|
+        product = num * num_2
+        if product.to_s == product.to_s.reverse
+          largest = product if product > largest
+        end
+      end
+    end
+    largest
+  end
+
+end
