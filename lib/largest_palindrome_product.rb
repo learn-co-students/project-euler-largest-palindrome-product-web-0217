ec9fce8c53ead360b15dd50b1fda3d5b0bc46709
hwalborn <hwalborn@gmail.com>
2017-05-16 12:38:12 -0400

    loops in loops

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,12 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+  palindromes = []
+  (900..999).each do |num_1|
+    (900...num_1).each do |num_2|
+      if((num_1 * num_2).to_s.split('') == (num_1 * num_2).to_s.split('').reverse)
+        palindromes.push(num_1 * num_2)
+      end
+    end
+  end
+  palindromes.max
+end
